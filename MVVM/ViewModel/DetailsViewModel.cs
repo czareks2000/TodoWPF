57a58b2866f77b03b77b068520c3d39ee2573b48
Justyna Sadowska <83035@student.pb.edu.pl>
2024-06-16 15:27:54 +0200

    naprawa przycisku do edycji

--- MVVM/ViewModel/DetailsViewModel.cs
+++ MVVM/ViewModel/DetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using Todo.Core;
 using Todo.DB;
@@ -18,34 +19,33 @@ namespace Todo.MVVM.ViewModel
             set
             {
                 _selectedTask = value;
-                
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(IsTaskEditable));
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsTaskEditable));
             }
         }
+
         public bool IsTaskEditable => SelectedTask != null && SelectedTask.Status != Model.Enums.TaskStatus.Done;
 
         public ICommand DeleteTaskCommand { get; private set; }
         public ICommand MarkAsCompletedCommand { get; private set; }
         public ICommand EditTaskCommand { get; private set; }
 
-        public DetailsViewModel() 
+        public DetailsViewModel()
         {
             _dataContext = new DataContext();
 
             DeleteTaskCommand = new RelayCommand(DeleteTask);
-            MarkAsCompletedCommand = new RelayCommand(MarkAsCompleted, obj=>IsTaskEditable);
-            EditTaskCommand = new RelayCommand(EditTask);
-
+            MarkAsCompletedCommand = new RelayCommand(MarkAsCompleted, obj => IsTaskEditable);
+            EditTaskCommand = new RelayCommand(EditTask, obj => IsTaskEditable);
         }
 
         private void DeleteTask(object obj)
         {
             MessageBoxResult result = MessageBox
                 .Show(
-                    "Czy na pewno chcesz usunąć to zadanie?", 
-                    "Potwierdzenie", 
-                    MessageBoxButton.YesNo, 
+                    "Do you want to delete this task?",
+                    "Confirm",
+                    MessageBoxButton.YesNo,
                     MessageBoxImage.Question
                 );
             if (result == MessageBoxResult.Yes)
@@ -59,27 +59,45 @@ namespace Todo.MVVM.ViewModel
 
         private void MarkAsCompleted(object obj)
         {
-            if(SelectedTask != null)
+            if (SelectedTask != null)
             {
-                SelectedTask.Status=Model.Enums.TaskStatus.Done;
+                SelectedTask.Status = Model.Enums.TaskStatus.Done;
                 _dataContext.SaveChanges();
                 OnPropertyChanged(nameof(SelectedTask));
                 OnPropertyChanged(nameof(IsTaskEditable));
+                CommandManager.InvalidateRequerySuggested(); // Invalidate commands
                 Mediator.Instance.Notify("UpdateTasksList", null);
                 Mediator.Instance.Notify("ChangeViewToAddTask", null);
-
             }
         }
-        private void EditTask(object obj)
+
+        private void MarkAsInProgress(object obj)
         {
-               Mediator.Instance.Notify("ChangeViewToEditTask", SelectedTask);
+            if (SelectedTask != null)
+            {
+                SelectedTask.Status = Model.Enums.TaskStatus.InProgress;
+                _dataContext.SaveChanges();
+                OnPropertyChanged(nameof(SelectedTask));
+                OnPropertyChanged(nameof(IsTaskEditable));
+                CommandManager.InvalidateRequerySuggested(); // Invalidate commands
+                Mediator.Instance.Notify("UpdateTasksList", null);
+            }
         }
 
-        private void CheckIfAllSubTasksComplete(object obj){
-            if(SelectedTask != null && SelectedTask.SubTasks.All(st=>st.Status==Model.Enums.TaskStatus.Done ))
-            { MarkAsCompleted(null); }
-           
+        private void EditTask(object obj)
+        {
+            Mediator.Instance.Notify("ChangeViewToEditTask", SelectedTask);
+            Mediator.Instance.Notify("UpdateTasksList", null);
         }
+
+        private void CheckIfAllSubTasksComplete(object obj)
+        {
+            if (SelectedTask != null && SelectedTask.SubTasks.All(st => st.Status == Model.Enums.TaskStatus.Done))
+            {
+                MarkAsCompleted(null);
+            }
+        }
+
         public void SubTaskChecked(object sender)
         {
             if (sender is SubTask subTask)
@@ -107,7 +125,10 @@ namespace Todo.MVVM.ViewModel
                     _dataContext.SaveChanges();
                 }
             }
-            CheckIfAllSubTasksComplete(null);
+            if (SelectedTask != null && SelectedTask.Status == Model.Enums.TaskStatus.Done)
+            {
+                MarkAsInProgress(null);
+            }
         }
     }
 }
